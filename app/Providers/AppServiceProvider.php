401f1040809c7d60431f4eabc1559f9a3097da6a
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2019-02-08 11:44:25 +0100

    feat(views): added blade directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Blade;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,9 +15,49 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Blade::directive('active', function ($expression) {
+            list($pattern, $class) = 
+                explode(
+                    ',', 
+                    $this->normalize($expression)
+                )
+            ;
+            return "<?= request()->is('$pattern') ? '$class' : ''; ?>";
+        }); 
+        
+        Blade::directive('errors', function ($expression) {
+            list($pattern, $class) = 
+                explode(
+                    ',', 
+                    $this->normalize($expression)
+                )
+            ;
+            
+            return '<?= $errors->has("' . $pattern . '") ? ' . $class . ' : ""; ?>';
+        }); 
+        
+        Blade::directive('route', function ($expression) {
+            $routeName = $this->normalize($expression);
+            return "<?= route('$routeName'); ?>";
+        }); 
     }
-
+    
+    /**
+     * Removes quotes & other characters passed to blade directives
+     * 
+     * When calling directives, the expressions given as parameters sometimes 
+     * are quoted, even though this is not necessary. 
+     * This method removes quotes, along with parentheses and 
+     * blank space characters, there by approximating the actual value to be used.
+     * 
+     * @param string $expression
+     * @return string
+     */
+    protected function normalize(string $expression): string
+    {
+        return str_replace(['(',')', ' ', "'"], '', $expression);
+    }
+    
     /**
      * Register any application services.
      *
