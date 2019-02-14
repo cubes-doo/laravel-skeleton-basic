7a901906c17333fac93fab789a5ce4534e0d0a75
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2019-02-14 11:26:20 +0100

    fix(views): navbar restructured
    To stay consistent with the material theme, the navbar needed some restructuring. The previous structure was copied & pasted from a previous project w/ custom css involved.

--- code/resources/views/_layout/partials/_navbar.blade.php
+++ code/resources/views/_layout/partials/_navbar.blade.php
@@ -1,8 +1,13 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
+<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top">
     <div class="container-fluid">
         <div class="navbar-wrapper">
-            <a class="navbar-brand" href="#pablo">Dashboard</a>
+            <div class="navbar-minimize">
+                <button id="minimizeSidebar" class="btn btn-just-icon btn-white btn-fab btn-round">
+                    <i class="material-icons text_align-center visible-on-sidebar-regular">more_vert</i>
+                    <i class="material-icons design_bullet-list-67 visible-on-sidebar-mini">view_list</i>
+                </button>
+            </div>
         </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="sr-only">Toggle navigation</span>
@@ -12,12 +17,41 @@
         </button>
         <div class="collapse navbar-collapse justify-content-end">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="#pablo">
-                        <i class="material-icons">notifications</i> Notifications
+
+                <li class="nav-item dropdown" style="display: none">
+                    <a class="nav-link buzz " href="http://example.com"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="material-icons ">notifications</i>
+                        <span class="notification ">5</span>
+                        <p class="d-lg-none d-md-block">
+                            Notification
+                        </p>
                     </a>
+                    <div class="dropdown-menu dropdown-menu-right all-notification" >
+                        <a class="dropdown-item" href="#">Mike John responded to your email</a>
+                        <a class="dropdown-item" href="#">You have 5 new tasks</a>
+                        <a class="dropdown-item" href="#">You're now friend with Andrew</a>
+                        <a class="dropdown-item" href="#">Another Notification</a>
+                        <a class="dropdown-item" href="#">Another One</a>
+                    </div>
                 </li>
-                <!-- your navbar here -->
+                <li class="nav-item dropdown d-none d-lg-block">
+                    <a class="nav-link" href="javascript:;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="material-icons">person</i>
+                          <p class="d-lg-none d-md-block">
+                            Account
+                          </p>
+                        <div class="ripple-container"></div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" >
+                        <a class="dropdown-item" href="javascript:;"><i class="material-icons mr-2">perm_identity</i>Edit Profile</a>
+                        <a class="dropdown-item" href="javascript:;"><i class="material-icons mr-2">lock</i>Change Password</a>
+                        <a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();"><i class="material-icons mr-2">power_settings_new</i>Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
