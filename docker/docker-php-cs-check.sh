438c33e171f1696570151fcad3bbd4222c235b4d
Aleksandar Dimic <aleksandar.dimic@cubes.rs>
2019-02-20 13:55:07 +0100

    feat: php-cs-fixer was added to the project

---
+++ docker/docker-php-cs-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $(dirname "$0")
+
+set -e errexit
+set -o pipefail
+set -a
+. ".env"
+set +a
+
+docker exec -u localuser -it ${COMPOSE_PROJECT_NAME}_phpfpm \
+	/opt/code/vendor/friendsofphp/php-cs-fixer/php-cs-fixer fix --dry-run --config=/opt/.php_cs
