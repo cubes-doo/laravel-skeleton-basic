c6f47d46c97ec17b4054d3d0dc6e6bf4c18caaf2
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2019-03-07 09:07:24 +0100

    feat: adding csrf token to meta tags

--- resources/views/_layout/partials/_head.blade.php
+++ resources/views/_layout/partials/_head.blade.php
@@ -2,6 +2,7 @@
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="csrf-token" content="{{ csrf_token() }}">
 @stack('meta')
 <!--     Fonts and icons     -->
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
