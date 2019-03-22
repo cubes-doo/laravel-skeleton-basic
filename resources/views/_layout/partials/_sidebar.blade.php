cb6ce9460a53248b561024d6d8eac566ea24cbd3
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2019-03-22 09:18:49 +0100

    feat(fe,sidebar): added smart collapse script
    Now when you change the sidebar state (normal/collapsed), it will be noted into local storage & persisted.

--- resources/views/_layout/partials/_sidebar.blade.php
+++ resources/views/_layout/partials/_sidebar.blade.php
@@ -1,52 +1,69 @@
 <!-- ========== Left Sidebar Start ========== -->
 <div class="left side-menu">
-        <div class="slimscroll-menu" id="remove-scroll">
+    <div class="slimscroll-menu" id="remove-scroll">
 
-            <!--- Sidemenu -->
-            <div id="sidebar-menu">
-                <!-- Left Menu Start -->
-                <ul class="metismenu list-unstyled" id="side-menu">
-                    <li class="menu-title">@lang('Navigation')</li>
-                    <li>
-                        <a class="@activeClass('dashboard', 'active')" href="{{route('dashboard')}}">
-                            <i class="fi-air-play"></i>
-                            <span class="badge badge-pill badge-success float-right">1</span> 
-                            <span>@lang('Dashboard')</span>
-                        </a>
-                    </li>
+        <!--- Sidemenu -->
+        <div id="sidebar-menu">
+            <!-- Left Menu Start -->
+            <ul class="metismenu list-unstyled" id="side-menu">
+                <li class="menu-title">@lang('Navigation')</li>
+                <li>
+                    <a class="@activeClass('dashboard', 'active')" href="{{route('dashboard')}}">
+                        <i class="fi-air-play"></i>
+                        <span class="badge badge-pill badge-success float-right">1</span> 
+                        <span>@lang('Dashboard')</span>
+                    </a>
+                </li>
 
-                    <li class="menu-title">@lang('More')</li>
+                <li class="menu-title">@lang('More')</li>
 
-                    <li>
-                        <a class="@activeClass('entities', 'active') @activeClass('entities/*', 'active')" href="{{route('entities.list')}}">
-                            <i class="fi-layers"></i>
-                            <span>@lang('Entities')</span>
-                        </a>
-                    </li>
+                <li>
+                    <a class="@activeClass('entities', 'active') @activeClass('entities/*', 'active')" href="{{route('entities.list')}}">
+                        <i class="fi-layers"></i>
+                        <span>@lang('Entities')</span>
+                    </a>
+                </li>
 
-                    <li>
-                        <a href="javascript: void(0);" aria-expanded="true"><i class="fi-share"></i> <span>Multi Level</span> <span class="menu-arrow"></span></a>
-                        <ul class="nav-second-level nav" aria-expanded="true">
-                            <li><a href="javascript: void(0);">Level 1.1</a></li>
-                            <li><a href="javascript: void(0);" aria-expanded="true">Level 1.2 <span class="menu-arrow"></span></a>
-                                <ul class="nav-third-level nav" aria-expanded="true">
-                                    <li><a href="javascript: void(0);">Level 2.1</a></li>
-                                    <li><a href="javascript: void(0);">Level 2.2</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
+                <li>
+                    <a href="javascript: void(0);" aria-expanded="true"><i class="fi-share"></i> <span>Multi Level</span> <span class="menu-arrow"></span></a>
+                    <ul class="nav-second-level nav" aria-expanded="true">
+                        <li><a href="javascript: void(0);">Level 1.1</a></li>
+                        <li><a href="javascript: void(0);" aria-expanded="true">Level 1.2 <span class="menu-arrow"></span></a>
+                            <ul class="nav-third-level nav" aria-expanded="true">
+                                <li><a href="javascript: void(0);">Level 2.1</a></li>
+                                <li><a href="javascript: void(0);">Level 2.2</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
 
 
-                </ul>
-
-            </div>
-            <!-- Sidebar -->
-            <div class="clearfix"></div>
+            </ul>
 
         </div>
-        <!-- Sidebar -left -->
+        <!-- Sidebar -->
+        <div class="clearfix"></div>
 
     </div>
-    <!-- Left Sidebar End -->
+    <!-- Sidebar -left -->
 
+</div>
+<!-- Left Sidebar End -->
+@push('footer_scripts')
+    <!-- end:sidebar script -->
+    <script type="text/javascript">
+        (function setSidebar() {
+            let sidebarCollapsed = localStorage.getItem('sidebarCollapsed');
+
+            if(sidebarCollapsed !== undefined && sidebarCollapsed !== 'false') {
+                $('body').addClass('enlarged');
+            }
+        })();
+
+        $('button.open-left').on('click', function(e){
+            localStorage.setItem('sidebarCollapsed', $("body").hasClass("enlarged"));
+            setSidebar();
+        });
+    </script>
+    <!-- end:sidebar script -->
+@endpush
