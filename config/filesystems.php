186d0325d5aaa384707660aea9b08845609b53cd
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2019-03-22 15:19:16 +0100

    refactor: now storing files in public/storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,12 +45,12 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => public_path('storage'),
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
