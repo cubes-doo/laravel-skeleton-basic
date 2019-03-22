a3ce0fd840458f5a0d0e7eb905ffa0e8cc15d38b
Aleksa Cvijic <aleksa.cvijic@cubes.rs>
2019-03-22 14:49:21 +0100

    feat(fe,ajax): showing sys-message on success & default one on error

--- resources/views/_layout/partials/ajax.blade.php
+++ resources/views/_layout/partials/ajax.blade.php
@@ -1,7 +1,24 @@
 <script type="text/javascript">
     $.ajaxSetup({
         headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+            'Accept': 'application/json'
+        },
+        success: function(response){
+            showSystemMessage(response.message);
+
+            if($('#datatables').DataTable()) {
+                $('#datatables').DataTable().draw();
+            } else {
+                console.log('no datatables to reload; must have id "datatables"');
+            }
+        },
+        error: function(){
+            Swal.fire({
+                title: "@lang('Ooops..')",
+                text: "@lang('An error occured when trying to execute this action')",
+                type: 'error'
+            });
         }
     });
 </script>
